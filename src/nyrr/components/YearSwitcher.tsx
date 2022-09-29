c6841b3712936e239353a899e5ac86bd804a804c
Ted Brakob <tedbrakob@gmail.com>
2022-09-28 20:13:40 -0400

    fixed formatting

--- src/nyrr/components/YearSwitcher.tsx
+++ src/nyrr/components/YearSwitcher.tsx
@@ -45,8 +45,8 @@ export default function YearSwitcher (props: Props) {
 
   return (
       <div className="bg-primary relative flex items-center justify-between h-11">
-      <div className="flex-1 flex items-center justify-center h-full">
-        <div className="flex flex-nowrap whitespace-nowrap overflow-x-auto w-[100vw] max-w-fit h-full hide-scrollbars">
+        <div className="flex-1 flex items-center justify-center h-full">
+          <div className="flex flex-nowrap whitespace-nowrap overflow-x-auto w-[100vw] max-w-fit h-full hide-scrollbars">
             {years.map((year) => {
               const isActive = props.year === year;              
               return(
