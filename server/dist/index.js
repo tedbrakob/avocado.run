0aff160b16c38493f6ff470e13acc4abcf3b1ef6
tedbrakob <tedbrakob@gmail.com>
2023-03-05 10:39:28 -0500

    24b542 - Update to use new Nyrr results api v2 (#27)

--- server/dist/index.js
+++ server/dist/index.js
@@ -22,15 +22,6 @@ var __importStar = (this && this.__importStar) || function (mod) {
     __setModuleDefault(result, mod);
     return result;
 };
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -38,17 +29,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv = __importStar(require("dotenv"));
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
-const nyrr_results_api_1 = __importDefault(require("nyrr-results-api"));
 dotenv.config();
 const app = (0, express_1.default)();
 const port = process.env.PORT;
 app.use((0, cors_1.default)({
     origin: '*',
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}));
-app.get('/nyrr-token', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const token = yield nyrr_results_api_1.default.getToken();
-    res.json({ "token": token });
 }));
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
